a8dc442cda12c1f665056502e5e2670ef20ec93d
sssemion <ssemi.on@yandex.ru>
2020-05-08 14:26:54 +0500

    secret_key

--- misschedule/__init__.py
+++ misschedule/__init__.py
@@ -1,10 +1,10 @@
-import base64
 import os
 
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = base64.b64encode(os.urandom(24)).decode("utf-8")
+
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "secret_key_123")
 
 from misschedule import controllers
 from misschedule import jinja_filters
